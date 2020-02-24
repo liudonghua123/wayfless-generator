aa2fe6c753f5dbb15762f58e6366c6cd93f3359d
liudonghua123 <liudonghua123@gmail.com>
2020-02-24 20:32:53 +0800

    Updates

---
+++ precache-manifest.9abd679cd0a421420bbb2b46814c9d72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62facc1879b4d71772cb1741e3aa4c58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95c5bfd18c1ba3cef6b0",
+    "url": "/static/css/2.06ffd1ac.chunk.css"
+  },
+  {
+    "revision": "f15354f18405658f0845",
+    "url": "/static/css/main.aba2f72f.chunk.css"
+  },
+  {
+    "revision": "95c5bfd18c1ba3cef6b0",
+    "url": "/static/js/2.c6d0461c.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.c6d0461c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15354f18405658f0845",
+    "url": "/static/js/main.a96dc281.chunk.js"
+  },
+  {
+    "revision": "ccf40110e94835a997e2",
+    "url": "/static/js/runtime-main.6e393375.js"
+  }
+]);
