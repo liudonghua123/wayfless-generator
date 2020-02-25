4182cdc5dd55f787b63aa44640d84a8f6d87324f
liudonghua123 <liudonghua123@gmail.com>
2020-02-25 13:34:14 +0800

    Updates

---
+++ precache-manifest.c52be52880daf808c5621de7cc327c70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dac0cb1498552891420640e70da5541",
+    "url": "/wayfless-generator/index.html"
+  },
+  {
+    "revision": "4305e917cdc483c8bfd3",
+    "url": "/wayfless-generator/static/css/2.019a8dfc.chunk.css"
+  },
+  {
+    "revision": "c63caed97c55b1aa4b2e",
+    "url": "/wayfless-generator/static/css/main.063a958c.chunk.css"
+  },
+  {
+    "revision": "4305e917cdc483c8bfd3",
+    "url": "/wayfless-generator/static/js/2.50b421f6.chunk.js"
+  },
+  {
+    "revision": "2d6d9ff39d9fb1470195f5fd5bc24fa7",
+    "url": "/wayfless-generator/static/js/2.50b421f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c63caed97c55b1aa4b2e",
+    "url": "/wayfless-generator/static/js/main.9ea19dd3.chunk.js"
+  },
+  {
+    "revision": "79c1981d005679742edf",
+    "url": "/wayfless-generator/static/js/runtime-main.6bfc5edd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wayfless-generator/precache-manifest.497feb8f8b6c720e1bebf20097a5e8a1.js"
+  "/wayfless-generator/precache-manifest.c52be52880daf808c5621de7cc327c70.js"
 );
 
 self.addEventListener('message', (event) => {
